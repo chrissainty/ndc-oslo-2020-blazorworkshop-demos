3e48ff93202d94707af06330c70400c1a31c1b15
Chris Sainty <chris@chrissainty.com>
2020-06-06 17:20:10 +0100

    wip

---
+++ Section-05/Section-05/Data/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Section_05.Data
+{
+    public class TodoItem
+    {
+        [Required]
+        public string Description { get; set; }
+        public int Importance { get; set; }
+    }
+}
