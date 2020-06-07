5046ba09a074ef20d21459374e1c3a21ca9621d9
Chris Sainty <chris@chrissainty.com>
2020-06-07 12:45:42 +0100

    wip

---
+++ Section-07/Section07/BlazorJSInterop/wwwroot/interop.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+window.blazorInterop = {
+    registerOnScrollListener: function (element) {
+        element.addEventListener("scroll", function (event) {
+            if (event.target) {
+                if (event.target.scrollHeight - event.target.offsetHeight - event.target.scrollTop < 1) {
+                    DotNet.invokeMethodAsync('BlazorJSInterop', 'TriggerNextLoad');
+                }
+            }
+        });
+    }
+}
